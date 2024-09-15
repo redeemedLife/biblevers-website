6e1ca4afe8e9c43a5fb733ded90d1f37a7a09e20
redeemed Life <stirbraul@gmail.com>
2024-09-15 15:21:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const verses = [
    
-   "Fürchte dich nicht, denn ich stehe dir bei; hab keine Angst, denn ich bin dein Gott! - Jesaja 41:10",
+    "Fürchte dich nicht, denn ich stehe dir bei; hab keine Angst, denn ich bin dein Gott! - Jesaja 41:10",
     "Er beschützt jeden, der ihm vertraut. - 1. Samuel 2:9",
     "Aber Gottes Barmherzigkeit ist groß. - Epheser 2:4-5",
     "Er gibt mir neue Kraft. - Psalm 23:3",
